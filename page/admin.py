7c6884dee4a4d7390b498c51b4323570e60e11ae
yusuffinan <yusufserhaat@gmail.com>
2023-11-29 23:18:50 +0300

    slug

--- page/admin.py
+++ page/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 from .models import Page
 # Register your models here.
 
-admin.site.register(Page)
+@admin.register(Page)
+class PageAdmin(admin.ModelAdmin):
+    list_display = ("pk","title", "status",)
+    prepopulated_fields = {"slug": ("title",),}
--- page/models.py
+++ page/models.py
@@ -8,9 +8,12 @@ STATUS = [
 
 class Page(models.Model):
     title = models.CharField(max_length=200)
-    # slug = models.SlugField()
+    slug = models.SlugField(default="", db_index=True, blank=True, unique=True, null= False, max_length=200)
     content = models.TextField()
-    cover_image = models.ImageField(upload_to="page")
+    cover_image = models.ImageField(blank=True, null=True, upload_to="page")
     status = models.CharField(default="draft", choices=STATUS, max_length=10)
     created_att = models.DateTimeField(auto_now_add=True)
     uploated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.title}"
