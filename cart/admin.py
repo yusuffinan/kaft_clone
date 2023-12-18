e09812918dd20bc9c27b2aec2f726ad6bdbb2d32
yusuffinan <yusufserhaat@gmail.com>
2023-12-18 14:41:13 +0300

    cart_models

--- cart/admin.py
+++ cart/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import ShopingCart, ShopingCartItem
 
-# Register your models here.
+
+admin.site.register(ShopingCart)
+admin.site.register(ShopingCartItem)
+
+# # Register your models here.
+# @admin.register(ShopingCart)
+# class ShoppingCartAdmin(admin.ModelAdmin):
+#     list_display = ("product", "price",)
+    
+# @admin.register(ShopingCartItem)
+# class ShoppingItemAdmin(admin.ModelAdmin):
+#     list_display = ("status",)
--- cart/models.py
+++ cart/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from product.models import Product
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save, m2m_changed
+from django.dispatch import receiver
 
 STATUS=[
     ('buyed','Alindi'),
@@ -11,15 +13,35 @@ STATUS=[
 
 class ShopingCartItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    price = models.FloatField()
+    price = models.FloatField(default=0)
     is_deleted = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     uploated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return f"{ self.product.title } price: { self.product.price }tl"
+    
 class ShopingCart(models.Model):
     user = models.ForeignKey(User, blank=True, null=True, on_delete=models.CASCADE)
     items= models.ManyToManyField(ShopingCartItem, blank=True)
-    total_price = models.FloatField()
+    total_price = models.FloatField(default=0)
     status = models.CharField(default="waiting", choices=STATUS, max_length=10,)
     created_at = models.DateTimeField(auto_now_add=True)
     uploated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return f" pk: { self.pk } - total: { self.total_price } - status: { self.status }"
+    
+
+@receiver(post_save, sender=ShopingCartItem)
+def shopping_cart_receiver(sender, instance, created, *args, **kwargs):
+        if created:
+             instance.price = instance.product.price
+             instance.save()
+        print(sender)
+        print(kwargs)
+
+@receiver(m2m_changed, sender=ShopingCart.items.through)
+def shopping_cart(sender, *args, **kwargs):
+     print(args)
+     print(kwargs)
