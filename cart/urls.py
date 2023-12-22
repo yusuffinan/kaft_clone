0cf7f5bebf5f0a70b590177091f400c041f405b4
yusuffinan <yusufserhaat@gmail.com>
2023-12-22 14:47:35 +0300

    Sepet_cart

--- cart/urls.py
+++ cart/urls.py
@@ -3,9 +3,12 @@ from django.urls import include, path
 from kaft_clone import settings
 from page.views import index
 from django.conf.urls.static import static
-from cart.views import shopping_cart_item_add
+from cart.views import shopping_cart_item_add, view_shopping_cart,remove_from_cart,add_back_to_cart
 
 urlpatterns = [
     path('<int:cart_item_id>/', shopping_cart_item_add, name='shopping_cart_item_add'),
+    path('view_shopping_cart/', view_shopping_cart, name='view_shopping_cart'),
+    path('remove_from_cart/<int:item_id>/', remove_from_cart, name='remove_from_cart'),
+    path('add_back_to_cart/<int:item_id>/', add_back_to_cart, name='add_back_to_cart'),
 ]
 
--- cart/views.py
+++ cart/views.py
@@ -16,3 +16,40 @@ def shopping_cart_item_add(request,cart_item_id):
         shopping_cart.total_price_update()
         shopping_cart.save()
     return redirect("/")
+
+def view_shopping_cart(request):
+    if request.user.is_authenticated:
+        user = request.user
+        shopping_cart = ShopingCart.objects.filter(user=user, status="waiting").last()
+        if shopping_cart:
+            return render(request, 'view_shopping_cart.html', {'shopping_cart': shopping_cart})
+    return render(request, 'view_shopping_cart.html') 
+
+def remove_from_cart(request, item_id):
+    if request.method == 'POST':
+        item = ShopingCartItem.objects.get(pk=item_id)
+        print(f"Removing item: {item}")
+        if not item.is_deleted:
+            item.is_deleted = True
+            item.save()
+            print(f"Item {item} is_deleted: {item.is_deleted}")
+            
+            shopping_cart_instance = item.shopingcart_set.first()
+            if shopping_cart_instance is not None:
+                shopping_cart_instance.total_price_update()
+                print(f"Total price updated: {shopping_cart_instance.total_price}")
+    
+    return redirect('view_shopping_cart')
+
+def add_back_to_cart(request, item_id):
+    if request.method == 'POST':
+        item = ShopingCartItem.objects.get(pk=item_id)
+        if item.is_deleted:
+            item.is_deleted = False
+            item.save()
+
+            shopping_cart_instance = item.shopingcart_set.first()
+            if shopping_cart_instance is not None:
+                shopping_cart_instance.total_price_update()
+
+    return redirect('view_shopping_cart')
